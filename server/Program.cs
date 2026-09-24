a2ae5ff4eb2f77cb07ad7be901b12117175ba0b3
krekerfit1 <kerenamd776@gmail.com>
2026-09-24 18:55:44 +0300

    добавил группы и даты

--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,6 @@
 ﻿
 Console.WriteLine("Приииииииииииииииивет я!");
 Console.WriteLine("Истомин Максим Алексеевич");
+Console.WriteLine("Группа: ИСП-243");
+Console.WriteLine($"дата и время: {DateTime.Now}");
 
